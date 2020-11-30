39c0d318b66b0f1d0148d6269f791997bb7007ed
Christian Kaye <44508564+christiankaye@users.noreply.github.com>
2020-11-30 16:21:33 -0600

    an example search
    to do: replace """ with "search term" (search query)

--- DelverTest.py
+++ DelverTest.py
@@ -23,23 +23,23 @@ class TestAll(unittest.TestCase):
         forms = c.forms(filters={'id': 'mplus-search'})
         search_form = forms[0]
         search_form.fields = {
-            'query': 'bursitis'
+            'query': """
         }
         c.submit(search_form)
         success_check = c.submit_check(
             search_form,
-            phrase='bursitis',
+            phrase= """,
             status_codes=[200]
         )
         print(success_check)
         
-        self.assertEqual(search_form.fields['query'].get('value'), 'bursitis')
+        self.assertEqual(search_form.fields['query'].get('value'), """)
 
     def test_crawler_scraper_methods(self):
         c = Crawler()
         c.logging = True
         c.useragent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
-        c.open("https://vsearch.nlm.nih.gov/vivisimo/cgi-bin/query-meta?v%3Aproject=medlineplus&v%3Asources=medlineplus-bundle&query=bursitis")
+        c.open("https://vsearch.nlm.nih.gov/vivisimo/cgi-bin/query-meta?v%3Aproject=medlineplus&v%3Asources=medlineplus-bundle&query=["""]")
         p_text = c.xpath('//*[@id="document-list"]/div[1]/div[2]/div[2]/p[1]')
 
 if __name__ == '__main__':

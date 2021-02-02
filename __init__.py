c49c9cac17a7b3c38723d4c4eb4c082acb94631e
Christian Kaye <44508564+christiankaye@users.noreply.github.com>
2021-02-02 17:28:24 -0600

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -16,76 +16,68 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # import libraries
 
 from mycroft import MycroftSkill, intent_file_handler
-from mycroft.util.parse imMedlinePlusSkill-masterport match_one
+from mycroft.util.parse import match_one
 from mycroft.audio import wait_while_speaking
-import requests
-import time
 import urllib
+from bs4 import BeautifulSoup
+from HTMLParser import HTMLParser
 import xml.etree.ElementTree
 
 class MedlinePlus(MycroftSkill):
     def __init__(self):
-    super(MedlinePlusSkill, self).__init__(name="MedlinePlusSkill")
-    
+        super(MedlinePlusSkill, self).__init__(name="MedlinePlusSkill")
     # Initialize working variables used within the skill.
-    self.count = 0
+    def initialize(self):
+        self.is_reading = False
     
-   @intent_handler(IntentBuilder().require("MedlinePlus"))
+    @intent_handler(IntentBuilder().require("MedlinePlus"))
     def handle_medlineplus_intent(self, message):
         self.speak_dialog("let_me_find")
         # medlineplus refers to the summary defined by the disease on medlineplus.gov
-			if message.data.get("MedlinePlus") is None:
+        if message.data.get("MedlinePlus") is None:
             response = self.get_response('notfound', num_retries=0)
             if response is None:
                return
-         else:
-            response = message.data.get("MedlinePlus")
+            else:
+                response = message.data.get("MedlinePlus")
         self.speak_dialog('found', data={"FullSummary": response}) 
 
 def remove_tags(text):
     return ''.join(xml.etree.ElementTree.fromstring("<dummy_tag>" + text +"</dummy_tag>").itertext())
 
-def medlinePlus_query(search_terms):
-    # Specify the base
-    root_url = 'https://wsearch.nlm.nih.gov/ws/query'
-    term_delimiter = '+'
-    keywords = term_delimiter.join(search_terms.split(' '))
+base_url = 'https://wsearch.nlm.nih.gov/ws/query?db=healthTopics&term='
+ret_type = '&rettype=brief'
 
-    search_url = '%s?db=%s&term="%s"' % (root_url, 'healthTopics', keywords)
-
-    # Create our results list which we'll populate.
+def MedlinePlus_query(search_terms):
+    term = "{0}".format(search_terms)
+    term = urllib.quote(term)
     results = []
+    url = (base_url + term + ret_type)
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
 
-    try:
-        # Connect to the server and read the response generated.
-        response = urllib.urlopen(search_url).read()
-        print (search_url) 
-        e = xml.etree.ElementTree.fromstring(response)
-        for doc in e.iter('document'):
-            title = ''
-            summary = ''
-            for content in doc.iter('content'):
-                if content.get('name') == 'title':
-                    title = content.text
-                elif content.get('name') == 'FullSummary':
-                    summary = content.text
+    for document in soup.findAll('document'):
+        contents = document.findAll('content')
+        for content in contents:
+            if content.get('name') == 'title':
+                taggedTitle = content.string
+                title = strip_tags(taggedTitle)
+            if content.get('name') == 'snippet':
+                taggedSnippet = content.string
+                snippet = strip_tags(taggedSnippet)
+                snippet = strip_tags(snippet)
+            if content.get('name') == 'FullSummary':
+                taggedSummary = content.string
+                summary = strip_tags(taggedSummary)
+        
+        results.append({
+            'title': remove_tags(title),
+            'link': document.get('url'),
+            'summary': remove_tags(snippet),
+            'content': remove_tags(summary),
+            'from': 'From:     MedlinePlus'
+        })
 
-            entry = {
-                'title': remove_tags(title),
-                'link': doc.get('url'),
-                'summary': remove_tags(summary)
-            }
-
-            results.append(entry) 
-        #print response 
-        #print search_url   
-
-    # Catch a URLError exception - something went wrong when connecting!
-    except urllib.URLError as e:
-        print ("Error when querying the MedlinePlus API: "), e
-
-    # Return the list of results to the calling function.
-    #print results
     return results
 
 def create_skill():

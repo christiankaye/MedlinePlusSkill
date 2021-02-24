b8b93cfa4e23ad37e7e197faed12fd2ee98ca45f
Christian Kaye <44508564+christiankaye@users.noreply.github.com>
2021-02-24 13:33:16 -0600

    changes

--- __init__.py
+++ __init__.py
@@ -1,21 +1,3 @@
-"""testmedlineplus URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
-from django.contrib import admin
-
 """
 skill medlineplus
 Copyright (C) 2021 The Nurse
@@ -36,67 +18,57 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.util.parse import match_one
 from mycroft.audio import wait_while_speaking
-import urllib
+import requests
 from bs4 import BeautifulSoup
-from HTMLParser import HTMLParser
-import xml.etree.ElementTree
+import time
+
 
 class MedlinePlus(MycroftSkill):
     def __init__(self):
-        super(MedlinePlusSkill, self).__init__(name="MedlinePlusSkill")
-    # Initialize working variables used within the skill.
-    def initialize(self):
-        self.is_reading = False
+    super(MedlinePlusSkill, self).__init__(name="MedlinePlusSkill")
     
+    # Initialize working variables used within the skill.
+    self.count = 0
+         
     @intent_handler(IntentBuilder().require("MedlinePlus"))
     def handle_medlineplus_intent(self, message):
         self.speak_dialog("let_me_find")
-        # medlineplus refers to the summary defined by the disease on medlineplus.gov
-        if message.data.get("MedlinePlus") is None:
+        # medlineplus refers to the topic-summary defined by the disease on medlineplus.gov
+			if message.data.get("MedlinePlus") is None:
             response = self.get_response('notfound', num_retries=0)
             if response is None:
                return
-            else:
-                response = message.data.get("MedlinePlus")
-        self.speak_dialog('found', data={"FullSummary": response}) 
+         else:
+            response = message.data.get("MedlinePlus")
+        self.speak_dialog('found', data={"disease": response})
+          else:
+            try:
+                url = "https://medlineplus.gov/{disease}.html"
+                html_content = requests.get(url).text
+                soup = BeautifulSoup(html_content, "html5lib")
+                main_class = soup.find("div",id="topic-summary")
+                link = main_class.find("a")
+                res = main_class.text
+                self.result.append(str(res))
+                           med
+	def get_soup(self, url):
+        try:
+            return BeautifulSoup(requests.get(url).text, "html.parser")
+        except Exception as SockException:
+            self.log.error(SockException)
 
-def remove_tags(text):
-    return ''.join(xml.etree.ElementTree.fromstring("<dummy_tag>" + text +"</dummy_tag>").itertext())
-
-base_url = 'https://wsearch.nlm.nih.gov/ws/query?db=healthTopics&term='
-ret_type = '&rettype=brief'
-
-def MedlinePlus_query(search_terms):
-    term = "{0}".format(search_terms)
-    term = urllib.quote(term)
-    results = []
-    url = (base_url + term + ret_type)
-    html = urllib.urlopen(url).read()
-    soup = BeautifulSoup(html)
-
-    for document in soup.findAll('document'):
-        contents = document.findAll('content')
-        for content in contents:
-            if content.get('name') == 'title':
-                taggedTitle = content.string
-                title = strip_tags(taggedTitle)
-            if content.get('name') == 'snippet':
-                taggedSnippet = content.string
-                snippet = strip_tags(taggedSnippet)
-                snippet = strip_tags(snippet)
-            if content.get('name') == 'FullSummary':
-                taggedSummary = content.string
-                summary = strip_tags(taggedSummary)
+    def get_summary(self, url):
+        soup = self.get_soup(url)
+        lines = [a.text.strip() for a in soup.find(id='summary-title')
+        return lines
         
-        results.append({
-            'title': remove_tags(title),
-            'link': document.get('url'),
-            'summary': remove_tags(snippet),
-            'content': remove_tags(summary),
-            'from': 'From:     MedlinePlus'
-        })
-
-    return results
+	def stop(self):
+        self.log.info('stop is called')
+        if self.is_reading is True:
+            self.is_reading = False
+            return True
+        else:
+            return False
 
 def create_skill():
     return MedlinePlusSkill()
